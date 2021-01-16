26de4801964848f3bd8255a93a46e834aa00b66f
Gustav Rasmussen <gcr84@hotmail.com>
2021-01-16 19:20:56 +0100

    Create dna_to_rna.ts

---
+++ src/dna_to_rna.ts
@@ -0,0 +1,11 @@
+
+var map = {"T": "A", "A": "U", "C": "G", "G": "C"};
+
+var DNA: string = "AACT";
+var RNA: string = "";
+
+for (let i=0; i < DNA.length; i++) {
+  RNA += map[DNA[i]]
+}
+
+console.log("RNA is: " + RNA)
