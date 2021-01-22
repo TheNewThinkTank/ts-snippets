70f598f935e91d131d7003022419adbc08d0c824
Gustav Rasmussen <gcr84@hotmail.com>
2021-01-22 14:27:21 +0100

    Update dna_to_rna.ts

--- src/dna_to_rna.ts
+++ src/dna_to_rna.ts
@@ -8,8 +8,21 @@ var map = {"T": "A", "A": "U", "C": "G", "G": "C"};
 var DNA: string = "AACT";
 var RNA: string = "";
 
+
+// Using for loop
 for (let i=0; i < DNA.length; i++) {
   RNA += map[DNA[i]]
 }
+
+console.log("RNA is: " + RNA)
+
+
+// Using forEach loop
+var DNA: string[] = ["A", "A", "C", "T"];
+var RNA: string = "";
+
+DNA.forEach(function (value) {
+    RNA += map[value]
+});
 
 console.log("RNA is: " + RNA)
