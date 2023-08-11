e8b452e90a7de6862fc7cd248311eaee2da28804
Gustav Rasmussen <gcr84@hotmail.com>
2023-08-11 13:03:25 +0200

    feat: add pseudo-code

--- src/kitchen-helper/make_shopping_list.ts
+++ src/kitchen-helper/make_shopping_list.ts
@@ -1,1 +1,11 @@
 // see README
+
+const INVENTORY = {
+    "apples": {"qty": 5},
+    "beef": {"amount": 470, "unit": "g"},
+    "tea": {"amount": 30, "unit": "g"},
+}
+
+const PEOPLE = 4
+
+const RECIPE = "tacos"
