eca682d211022f026bd68e4ee8eaa79712c21381
Gustav Rasmussen <gcr84@hotmail.com>
2024-04-01 16:53:39 +0200

    fix: remove lint

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,57 @@
-
 // Object Destructuring
 
-var rect = { x: 0, y: 10, width: 15, height: 20 };
+const rect = { x: 0, y: 10, width: 15, height: 20 }
 
 // Destructuring assignment
-var {x, y, width, height} = rect;
-console.log(x, y, width, height); // 0,10,15,20
-
+const { x, y, width, height } = rect
+console.log(x, y, width, height)  // 0,10,15,20
 
 // Spread Operator
 
 // Apply
-function foo(x, y, z) { }
-var args = [0, 1, 2];
-foo(...args);
+function foo (x, y, z) { }
+let args = [0, 1, 2]
+foo(...args)
 
 // Destructuring
-var [x, y, ...remaining] = [1, 2, 3, 4];
-console.log(x, y, remaining); // 1,2,[3,4]
+var [x, y, ...remaining] = [1, 2, 3, 4]
+console.log(x, y, remaining)  // 1,2,[3,4]
 
 // Array Assignment
-var list = [1, 2];
-list = [...list, 3, 4];
-console.log(list); // [1,2,3,4]
+var list = [1, 2]
+list = [...list, 3, 4]
+console.log(list)  // [1,2,3,4]
 
 // Object spread
-const point2D = {x: 1, y: 2};
+const point2D = {x: 1, y: 2}
 /** Create a new object by using all the point2D props along with z */
-const point3D = {...point2D, z: 3};
-
+const point3D = {...point2D, z: 3}
 
 // for...of
 
 var someArray = [9, 2, 5];
 for (var item in someArray) {
-    console.log(item); // 0,1,2
+    console.log(item)  // 0,1,2
 }
 
-var someArray = [9, 2, 5];
+var someArray = [9, 2, 5]
 for (var item of someArray) {
-    console.log(item); // 9,2,5
+    console.log(item)  // 9,2,5
 }
 
 var hello = "is it me you're looking for?";
 for (var char of hello) {
-    console.log(char); // is it me you're looking for?
+    console.log(char)  // is it me you're looking for?
 }
 
 // template literals
 
 // String Interpolation
-var lyrics = 'Never gonna give you up';
-var html = `<div>${lyrics}</div>`;
+var lyrics = 'Never gonna give you up'
+var html = `<div>${lyrics}</div>`
 
 // Multiline Literals
 var lyrics = `Never gonna give you up
-Never gonna let you down`;
+Never gonna let you down`
 
 // Tagged Templates
