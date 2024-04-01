d1904d710e15072954a0b8537bc17823a4d4379e
Gustav Rasmussen <gcr84@hotmail.com>
2024-04-01 16:28:33 +0200

    fix: bump up checkout action version

--- src/dna_to_rna.ts
+++ src/dna_to_rna.ts
@@ -8,14 +8,12 @@ var map = {"T": "A", "A": "U", "C": "G", "G": "C"};
 var DNA: string = "AACT";
 var RNA: string = "";
 
-
 // Using for loop
 for (let i=0; i < DNA.length; i++) {
   RNA += map[DNA[i]]
 }
 
 console.log("RNA is: " + RNA)
-
 
 // Using forEach loop
 var DNA: string[] = ["A", "A", "C", "T"];
