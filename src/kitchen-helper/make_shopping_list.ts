8e17e126468166f7e24e94420c1a974d2fd68695
Gustav Rasmussen <gcr84@hotmail.com>
2024-04-01 16:56:09 +0200

    fix: remove lint

--- src/kitchen-helper/make_shopping_list.ts
+++ src/kitchen-helper/make_shopping_list.ts
@@ -1,25 +1,22 @@
 // see README
 
-
 // TODO: nested map
-let INVENTORY = new Map();
+const INVENTORY = new Map()
 
-INVENTORY.set('apples', {"qty": 5});
-INVENTORY.set('beef', {"amount": 470, "unit": "g"});
-INVENTORY.set('tea', {"amount": 30, "unit": "g"});
+INVENTORY.set('apples', { "qty": 5 })
+INVENTORY.set('beef', { "amount": 470, "unit": "g" })
+INVENTORY.set('tea', { "amount": 30, "unit": "g" })
 
 // To verify if a key exists.
 // Outputs: true
-console.log(INVENTORY.has('apples'));
-
-console.log("Number of apples: " + INVENTORY.get('apples'));
+console.log(INVENTORY.has('apples'))
+console.log(`Number of apples: ${INVENTORY.get('apples')}`)
 
 // const INVENTORY = {
-//     "apples": {"qty": 5},
-//     "beef": {"amount": 470, "unit": "g"},
-//     "tea": {"amount": 30, "unit": "g"},
+//     "apples": { "qty": 5 },
+//     "beef": { "amount": 470, "unit": "g" },
+//     "tea": { "amount": 30, "unit": "g" },
 // }
-
 
 // const PEOPLE = 4
 // const RECIPE = "tacos"
