dcf82cf9549ef4b86d5040dcc3d132b036ce999e
Gustav Rasmussen <gcr84@hotmail.com>
2024-04-01 16:48:14 +0200

    chore: extend class

--- src/dna_to_rna.ts
+++ src/dna_to_rna.ts
@@ -1,25 +1,26 @@
 export class DNAtoRNA {
 
+  const map = { T: 'A', A: 'U', C: 'G', G: 'C' }
+
+  const DNA: string = 'AACT'
+  let RNA: string = ''
+
+  for (let i = 0; i < DNA.length; i++) {
+    RNA += map[DNA[i]]
+  }
+
+  console.log('RNA is: ' + RNA)
+
+  const dna: string[] = ['A', 'A', 'C', 'T']
+  const rna: string = ''
+
+  console.log(`dna is: ${dna}`)
+  console.log(`rna is: ${rna}`)
+
+  DNA.forEach(function (value) {
+    RNA += map[value]
+  })
+
+  console.log(`RNA is: ${RNA}`)
+
 }
-
-const map = { T: 'A', A: 'U', C: 'G', G: 'C' }
-
-let DNA: string = 'AACT'
-let RNA: string = ''
-
-// Using for loop
-for (let i = 0; i < DNA.length; i++) {
-  RNA += map[DNA[i]]
-}
-
-console.log('RNA is: ' + RNA)
-
-// Using forEach loop
-let dna: string[] = ['A', 'A', 'C', 'T']
-let rna: string = ''
-
-DNA.forEach(function (value) {
-  RNA += map[value]
-})
-
-console.log('RNA is: ' + RNA)
