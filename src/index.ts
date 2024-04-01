bd35135cfba4cbf67cfda74101b979d30584942d
Gustav Rasmussen <gcr84@hotmail.com>
2024-04-01 16:59:42 +0200

    fix: remove lint

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const rect = { x: 0, y: 10, width: 15, height: 20 }
 
 // Destructuring assignment
 const { x, y, width, height } = rect
-console.log(x, y, width, height)  // 0,10,15,20
+console.log(x, y, width, height) // 0,10,15,20
 
 // Spread Operator
 
@@ -15,12 +15,12 @@ foo(...args)
 
 // Destructuring
 var [x, y, ...remaining] = [1, 2, 3, 4]
-console.log(x, y, remaining)  // 1,2,[3,4]
+console.log(x, y, remaining) // 1,2,[3,4]
 
 // Array Assignment
 var list = [1, 2]
 list = [...list, 3, 4]
-console.log(list)  // [1,2,3,4]
+console.log(list) // [1,2,3,4]
 
 // Object spread
 const point2D = {x: 1, y: 2}
@@ -31,17 +31,17 @@ const point3D = {...point2D, z: 3}
 
 var someArray = [9, 2, 5];
 for (var item in someArray) {
-    console.log(item)  // 0,1,2
+    console.log(item) // 0,1,2
 }
 
 var someArray = [9, 2, 5]
 for (var item of someArray) {
-    console.log(item)  // 9,2,5
+    console.log(item) // 9,2,5
 }
 
 var hello = "is it me you're looking for?";
 for (var char of hello) {
-    console.log(char)  // is it me you're looking for?
+    console.log(char) // is it me you're looking for?
 }
 
 // template literals
