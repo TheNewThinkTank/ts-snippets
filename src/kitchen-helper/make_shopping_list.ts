e577ecc2745760ba4925fbf70ba74442b68a02a5
Gustav Rasmussen <gcr84@hotmail.com>
2024-04-01 16:58:20 +0200

    fix: remove lint

--- src/kitchen-helper/make_shopping_list.ts
+++ src/kitchen-helper/make_shopping_list.ts
@@ -3,9 +3,9 @@
 // TODO: nested map
 const INVENTORY = new Map()
 
-INVENTORY.set('apples', { "qty": 5 })
-INVENTORY.set('beef', { "amount": 470, "unit": "g" })
-INVENTORY.set('tea', { "amount": 30, "unit": "g" })
+INVENTORY.set('apples', { qty: 5 })
+INVENTORY.set('beef', { amount: 470, unit: 'g' })
+INVENTORY.set('tea', { amount: 30, unit: 'g' })
 
 // To verify if a key exists.
 // Outputs: true
@@ -13,10 +13,10 @@ console.log(INVENTORY.has('apples'))
 console.log(`Number of apples: ${INVENTORY.get('apples')}`)
 
 // const INVENTORY = {
-//     "apples": { "qty": 5 },
-//     "beef": { "amount": 470, "unit": "g" },
-//     "tea": { "amount": 30, "unit": "g" },
+//     'apples': { 'qty': 5 },
+//     'beef': { 'amount': 470, 'unit': 'g' },
+//     'tea': { 'amount': 30, 'unit': 'g' },
 // }
 
 // const PEOPLE = 4
-// const RECIPE = "tacos"
+// const RECIPE = 'tacos'
