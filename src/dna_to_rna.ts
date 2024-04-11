c97ecef7370892f7c8b75f860f168eadaf9e0b97
Gustav Rasmussen <gcr84@hotmail.com>
2024-04-11 19:07:13 +0200

    fix: remove lint

--- src/dna_to_rna.ts
+++ src/dna_to_rna.ts
@@ -1,26 +1,26 @@
-export class DNAtoRNA {
+// export class DNAtoRNA {
 
-  const map = { T: 'A', A: 'U', C: 'G', G: 'C' }
+//   const map = { T: 'A', A: 'U', C: 'G', G: 'C' }
 
-  const DNA: string = 'AACT'
-  let RNA: string = ''
+//   const DNA: string = 'AACT'
+//   // let RNA: string = ''
 
-  for (let i = 0; i < DNA.length; i++) {
-    RNA += map[DNA[i]]
-  }
+//   // for (let i = 0; i < DNA.length; i++) {
+//   //   RNA += map[DNA[i]]
+//   // }
 
-  console.log('RNA is: ' + RNA)
+//   // console.log('RNA is: ' + RNA)
 
-  const dna: string[] = ['A', 'A', 'C', 'T']
-  const rna: string = ''
+//   const dna: string[] = ['A', 'A', 'C', 'T']
+//   const rna: string = ''
 
-  console.log(`dna is: ${dna}`)
-  console.log(`rna is: ${rna}`)
+//   // console.log(`dna is: ${dna}`)
+//   // console.log(`rna is: ${rna}`)
 
-  DNA.forEach(function (value) {
-    RNA += map[value]
-  })
+//   // DNA.forEach(function (value) {
+//   //   RNA += map[value]
+//   // })
 
-  console.log(`RNA is: ${RNA}`)
+//   // console.log(`RNA is: ${RNA}`)
 
-}
+// }
